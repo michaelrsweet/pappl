a1bbf3aa7793c8332e0331ee6f2905ef6c47a9e4
Michael R Sweet <michael.r.sweet@gmail.com>
2020-03-27 13:50:04 -0400

    Add test suite framework.

---
+++ testsuite/pwg-driver.h
@@ -0,0 +1,14 @@
+//
+//  file for the Printer Application Framework
+//
+// Copyright © 2019-2020 by Michael R Sweet.
+//
+// Licensed under Apache License v2.0.  See the file "LICENSE" for more
+// information.
+//
+
+//
+// Include necessary headers...
+//
+
+#include <pappl/pappl.h>

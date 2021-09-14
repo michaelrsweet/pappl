78dc7f4ac30401cdc045cadcf57289b969619766
Michael R Sweet <michael.r.sweet@gmail.com>
2021-09-14 15:01:20 -0400

    Fix libcups DLL location

--- vcnet/copy-dlls.bat
+++ vcnet/copy-dlls.bat
@@ -1,5 +1,5 @@
 :: Script to copy dependent DLLs to the named build directory
-copy packages\libcups2_native.redist.2.4.0-b5\build\native\bin\x64\Debug\*.dll %1
+copy packages\libcups2_native.redist.2.4.0\build\native\bin\x64\Debug\*.dll %1
 copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Debug\*.dll %1
 copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Debug\*.dll %1
 copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll %1

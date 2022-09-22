87cc0a77b9c9c1f57af9edc98264faa819956fb2
Michael R Sweet <michael.r.sweet@gmail.com>
2022-09-22 08:04:07 -0400

    Bump libcups2_native version.

--- vcnet/copy-dlls.bat
+++ vcnet/copy-dlls.bat
@@ -1,5 +1,5 @@
 :: Script to copy dependent DLLs to the named build directory
-copy packages\libcups2_native.redist.2.4.3.3\build\native\bin\x64\Debug\*.dll %1
+copy packages\libcups2_native.redist.2.4.3.4\build\native\bin\x64\Debug\*.dll %1
 copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Debug\*.dll %1
 copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Debug\*.dll %1
 copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll %1

b6d7501940d6fe89b57ebbdc876e79b0f2238fba
Michael R Sweet <michael.r.sweet@gmail.com>
2022-12-26 12:54:08 -0500

    Bump version.

--- vcnet/config.h
+++ vcnet/config.h
@@ -107,7 +107,7 @@ typedef unsigned long useconds_t;
 
 
 // Version numbers
-#define PAPPL_VERSION "1.3.0"
+#define PAPPL_VERSION "1.3.1"
 #define PAPPL_VERSION_MAJOR 1
 #define PAPPL_VERSION_MINOR 3
 

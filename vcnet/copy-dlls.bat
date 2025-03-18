8a11e59d46d8243dc005adc015d73f6c42a649c8
Michael R Sweet <msweet@msweet.org>
2025-03-18 16:12:04 -0400

    Update copy-dlls.bat for new DLLs.

--- vcnet/copy-dlls.bat
+++ vcnet/copy-dlls.bat
@@ -1,8 +1,3 @@
 :: Script to copy dependent DLLs to the named build directory
-copy packages\libcups3_native.redist.3.0.0-rc3\build\native\bin\x64\Debug\*.dll %1
-copy packages\libjpeg-turbo-v142.2.0.4.3\build\native\bin\x64\v142\Debug\*.dll %1
-copy packages\libressl_native.redist.3.7.3\build\native\bin\x64\Debug\*.dll %1
-copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Debug\*.dll %1
-copy packages\libpng_native.redist.1.6.30\build\native\bin\x64\Release\*.dll %1
-copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Debug\*.dll %1
-copy packages\zlib_native.redist.1.2.11\build\native\bin\x64\Release\*.dll %1
+copy packages\*\build\native\bin\x64\Release\*.dll %1
+copy packages\*\build\native\bin\x64\Debug\*.dll %1

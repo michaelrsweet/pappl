8e8ea42608c00d7ec232a58b0a3e98e3af77e29c
Michael R Sweet <msweet@msweet.org>
2025-11-10 15:21:56 -0500

    Fix default IPv6 listener (Issue #401)

--- pappl/system-accessors.c
+++ pappl/system-accessors.c
@@ -187,8 +187,8 @@ papplSystemAddListeners(
 
     if (system->port)
     {
-      ret = add_listeners(system, name, system->port, AF_INET) ||
-            add_listeners(system, name, system->port, AF_INET6);
+      ret =  add_listeners(system, name, system->port, AF_INET);
+      ret |= add_listeners(system, name, system->port, AF_INET6);
     }
     else
     {
@@ -210,7 +210,7 @@ papplSystemAddListeners(
       if (ret)
       {
         system->port = port;
-        add_listeners(system, name, port, AF_INET6);
+        ret |= add_listeners(system, name, port, AF_INET6);
       }
     }
   }

69a3b543238c17854008a8427286eb0403f03abe
Michael R Sweet <msweet@msweet.org>
2026-09-26 17:11:47 -0400

    Fix unterminated OID buffer (Issue #440)

--- pappl/snmp.c
+++ pappl/snmp.c
@@ -816,11 +816,15 @@ asn1_get_oid(
 
   if (*buffer >= bufend)
   {
+    *oid = -1;
+
     return (0);
   }
   else if (length > (unsigned)(bufend - *buffer))
   {
+    *oid    = -1;
     *buffer = bufend;
+
     return (0);
   }
 
